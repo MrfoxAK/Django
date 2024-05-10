6fabd27cc8688ce3086632ebe1b39a89c29db9ba
Akash <akd270302@gmail.com>
2024-05-10 20:36:19 +0530

    Ecommerce Layout

--- E_Commerce/shop/views.py
+++ E_Commerce/shop/views.py
@@ -4,3 +4,36 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
      return render(request, 'shop/index.html')
+
+
+def about(request):
+     return HttpResponse("We are in About")
+
+def contact(request):
+     return HttpResponse("We are in Contact")
+
+def tracker(request):
+     return HttpResponse("We are in tracker")
+
+def search(request):
+     return HttpResponse("We are in search")
+
+def productView(request):
+     return HttpResponse("We are in product view")
+
+def checkout(request):
+     return HttpResponse("We are in checkout")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
